8dcaece17893f7d4e9aeab3be51b138e6b649f01
Ryokenka <remi.guerin99@gmail.com>
2024-06-08 15:11:05 +0200

    Increase Timeout

--- app/src/main/java/fr/epf/min1/projetandroid/CountryRepository.kt
+++ app/src/main/java/fr/epf/min1/projetandroid/CountryRepository.kt
@@ -1,16 +1,29 @@
 package fr.epf.min1.projetandroid
 
 import kotlinx.coroutines.CoroutineScope
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class CountryRepository(private val coroutineScope: CoroutineScope) {
     private val api: ApiService
 
     init {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://restcountries.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
         api = retrofit.create(ApiService::class.java)
     }
